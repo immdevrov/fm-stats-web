fbe236a2b24973e8be3da35d3e575dfccb7dee4a
Roman <immdevrov@gmail.com>
2026-04-07 03:21:18 +0400

    chore: silo local config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig(() => ({
   base: process.env.CI ? '/fm-stats-web/' : '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   plugins: [
     react({
       babel: {
